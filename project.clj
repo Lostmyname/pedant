ce7eb40bc7f90f98d3cf199c20958c4666589a54
Tom Cartwright <tecartwright@gmail.com>
2015-01-20 20:44:01 +0000

    Moving main class to make building for heroku easier

--- project.clj
+++ project.clj
@@ -1,15 +1,14 @@
 (defproject pedant "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Post a checklist to a pull request"
+  :url "https://github.com/Lostmyname/pedant"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [clj-http "1.0.1"]
                  [environ "1.0.0"]
+                 [ring/ring-jetty-adapter "1.3.2"]
                  [ring/ring-json "0.3.1"]
                  [ring/ring-defaults "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]]
-  :ring {:handler pedant.core.handler/app}
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  :ring {:handler pedant.web/app}
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"] [ring-mock "0.1.5"]]}})
--- src/pedant/web.clj
+++ src/pedant/web.clj
@@ -1,10 +1,12 @@
-(ns pedant.core.handler
+(ns pedant.web
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [compojure.handler :refer [site]]
             [compojure.handler :as handler]
             [ring.middleware.json :as middleware]
             [clj-http.client :as client]
             [environ.core :refer [env]]
+            [ring.adapter.jetty :as jetty]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]))
 
 (defn send-checklist [pr-url]
@@ -22,6 +24,7 @@
   {:status 200})
 
 (defroutes app-routes
+  (GET "/" [] {:status 200 :body "Solid"})
   (POST "/pull-request" {body :body} (create-pr-list body))
   (route/not-found "Not Found"))
 
@@ -29,3 +32,7 @@
   (-> (handler/api app-routes)
       (middleware/wrap-json-body)
       (middleware/wrap-json-response)))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
